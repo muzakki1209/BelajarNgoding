944831ee9f8536fd91cf6035efc777bfd04956ad
muzakki <muzakki120903@gmail.com>
2023-11-30 21:50:47 +0700

    upload

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ for(let i=0; i<5; i++){
     }
     console.log(bintang)
 }
+for(let i=3; i>0; i--){
+    let bintang=""
+    for(let j=0; j<i; j++){
+        bintang+="*"
+    }
+    console.log(bintang)
+}
