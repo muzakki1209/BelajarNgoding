5dba2c05674f90d1f1592702656f3a794fa01cf6
muzakki <muzakki120903@gmail.com>
2023-12-07 20:52:13 +0700

    upload

---
+++ tanggal.js
@@ -0,0 +1,15 @@
+let hari=12
+let bulan='september'
+let tahun=2003
+
+switch (2) {
+    case 1:
+        console.log(hari,bulan,tahun)
+        break;
+    case 2:
+        console.log(tahun,bulan,hari)
+        break;
+
+    default:
+        break;
+}
