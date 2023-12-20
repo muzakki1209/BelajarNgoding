08de0b609c9366e275fa2eb959bd58a37ce4abdf
muzakki <muzakki120903@gmail.com>
2023-12-20 15:14:37 +0700

    upload tugas

---
+++ word.js
@@ -0,0 +1,72 @@
+// soal 1
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+// ouput soal 1
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+
+// Enter tiap soal
+console.log(' ');
+
+// soal 2
+let word2 = 'wow JavaScript is so cool';
+let FirstWord = word2[0] + word2[1] + word2[2];
+let SecondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13];
+let ThirdWord = word2[15] + word2[16];
+let FourthWord = word2[18] + word2[19];
+let FifthWord = word2[21] + word2[22] + word2[23] + word2[24];
+
+// output soal 2
+console.log('First Word: ' + FirstWord);
+console.log('Second Word: ' + SecondWord);
+console.log('Third Word: ' + ThirdWord);
+console.log('Fourth Word: ' + FourthWord);
+console.log('Fifth Word: ' + FifthWord);
+
+// Enter tiap soal
+console.log(' ');
+
+// Soal 3
+let word3 = 'wow JavaScript is so cool';
+let FirstWord3 = word3.substring(0, 3);
+let SecondWord3 = word3.substring(4, 14);
+let ThirdWord3 = word3.substring(15, 17);
+let FourthWord3 = word3.substring(18, 20);
+let FifthWord3 = word3.substring(21, 25);
+
+// Ouput Soal 3
+console.log('First Word: ' + FirstWord3);
+console.log('Second Word: ' + SecondWord3);
+console.log('Third Word: ' + ThirdWord3);
+console.log('Fourth Word: ' + FourthWord3);
+console.log('Fifth Word: ' + FifthWord3);
+
+// Enter tiap soal
+console.log(' ');
+
+// soal 4
+let word4 = 'wow JavaScript is so cool';
+let FirstWord4 = word4.substring(0, 3);
+let SecondWord4 = word4.substring(4, 14);
+let ThirdWord4 = word4.substring(15, 17);
+let FourthWord4 = word4.substring(18, 20);
+let FifthWord4 = word4.substring(21, 25);
+
+// menghitung panjang string
+let firstWordLength = FirstWord4.length;
+let secondWordLength = SecondWord4.length;
+let thirdWordLength = ThirdWord4.length;
+let fourthWordLength = FourthWord4.length;
+let fifthWordLength = FifthWord4.length;
+
+// output soal 4
+console.log('First Word: ' + FirstWord4 + ', with length: ' + firstWordLength);
+console.log('First Word: ' + SecondWord4 + ', with length: ' + secondWordLength);
+console.log('First Word: ' + ThirdWord4 + ', with length: ' + thirdWordLength);
+console.log('First Word: ' + FourthWord4 + ', with length: ' + fourthWordLength);
+console.log('First Word: ' + FifthWord4 + ', with length: ' + fifthWordLength);
